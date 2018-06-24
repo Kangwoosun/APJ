9e37a0fe8e93497adc106c13a5315148c62f5554
KangWooSun <kws981924@gmail.com>
2018-06-24 14:04:22 +0900

    Add_Edit_Delete_Dialog, Doing's comment complete

--- mini_project/Doing.java
+++ mini_project/Doing.java
@@ -13,7 +13,9 @@ public class Doing implements Comparable<Doing>, Serializable{
 		this.timeTo = timeTo;
 		this.toDo = toDo;
 	}
-	
+	/*
+	 *  generalic get & set function
+	 */
 	public int getTimeFrom() {
 		return this.timeFrom;
 	}
@@ -33,17 +35,21 @@ public class Doing implements Comparable<Doing>, Serializable{
 		this.toDo = toDo;
 	}
 	
+	// Using when display at jlist
 	@Override
 	public String toString() {
 		return String.format("%02d:%02d - %02d:%02d %40s %s", timeFrom/60, timeFrom%60,
 				timeTo/60, timeTo%60, new String("") ,toDo);
 	}
 	
+	// Using when sort listDoing
 	@Override
 	public int compareTo(Doing anObject) {
-		//시작시간이 같은 경우 끝시간을 비교함
+		
+		//If each start time are same, compare end time
 		if(this.getTimeFrom() == anObject.getTimeFrom()) {
-			//끝시간이 같을 경우 할일 비교
+			
+			//If each end time are same, compare ToDo
 			if(this.getTimeTo() == anObject.getTimeTo()) {
 				return this.getToDo().compareTo(anObject.getToDo());
 			}
@@ -52,13 +58,14 @@ public class Doing implements Comparable<Doing>, Serializable{
 				else return -1;
 			}
 		}
-		//시작시간이 다를경우 시작시간을 비교함
+		//If each start time are different, compare start time
 		else {
 			if(this.getTimeFrom() > anObject.getTimeFrom()) return 1;
 			else return -1;
 		}
 			
 	}
+	// Using when compare originlistdoing and returned listdoing
 	@Override
 	public boolean equals(Object anObject) {
 		if (anObject instanceof Doing)
